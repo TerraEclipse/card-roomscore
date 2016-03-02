16efdb0c38b2589f350d1cf968ec0f47e83953b4
Nick Badagliacca <nick@terraeclipse.com>
2016-03-02 13:47:22 -0800

    Don't use query strings

--- js/bg.js
+++ js/bg.js
@@ -4,8 +4,9 @@ $(function () {
     var bg = document.getElementsByClassName('background')[0]
     var state = card.getAttribute('data-state')
     var number = Math.floor(Math.random() * 3) + 1
+    var css = 'background-image: url(https://s3-us-west-2.amazonaws.com/roomscore/states-cropped/' + state + '-' + number + '.jpg)'
 
-    bg.setAttribute('style', `background-image: url(https://s3-us-west-2.amazonaws.com/roomscore/states-cropped/${state}-${number}.jpg)`)
+    bg.setAttribute('style', css)
   }
   setBackground()
 });
